9558d3dcbc453a6cd639edd2e8cb21128c34ae20
vidura <vidura200427@gmail.com>
2025-10-31 10:02:31 +0530

    feat: error handling middleware done

--- config/env.js
+++ config/env.js
@@ -2,6 +2,6 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-const { PORT, NODE_ENV, DB_URI } = process.env;
+const { PORT, NODE_ENV, DB_URI , JWT_SECRET , JWT_EXPIRES_IN} = process.env;
 
-export { PORT, NODE_ENV, DB_URI };
+export { PORT, NODE_ENV, DB_URI ,JWT_SECRET , JWT_EXPIRES_IN};  
