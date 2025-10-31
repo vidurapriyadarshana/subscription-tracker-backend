f21374fbfbc51548b0e245045085edff46f7c450
vidura <vidura200427@gmail.com>
2025-10-31 09:48:42 +0530

    feat: models done

--- config/env.js
+++ config/env.js
@@ -2,6 +2,6 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-const { PORT, NODE_ENV , DB_URI } = process.env;
+const { PORT, NODE_ENV, DB_URI } = process.env;
 
 export { PORT, NODE_ENV, DB_URI };
---
+++ models/subscription.model.js
@@ -0,0 +1,99 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Subscription Name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 100,
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price must be greater than 0"],
+    },
+    currency: {
+      type: String,
+      enum: ["USD", "EUR", "LKR"],
+      default: "LKR",
+    },
+    frequency: {
+      type: String,
+      enum: ["daily", "weekly", "monthly", "yearly"],
+    },
+    category: {
+      type: String,
+      enum: [
+        "sports",
+        "news",
+        "entertainment",
+        "education",
+        "productivity",
+        "other",
+      ],
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "canceled", "expired"],
+      default: "active",
+    },
+    startDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: (value) => value <= new Date(),
+        message: "Start Date must be in the past",
+      },
+    },
+    renewalDate: {
+      type: Date,
+      validate: {
+        validator: function (value) {
+          return value > this.startDate;
+        },
+        message: "Renewal Date must be after Start Date",
+      },
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+subscriptionSchema.pre("save", function () {
+  if (!this.renewalDate) {
+    const renewalPeriods = {
+      daily: 1,
+      weekly: 7,
+      monthly: 30,
+      yearly: 365,
+    };
+
+    this.renewalDate = new Date(this.startDate);
+    this.renewalDate.setDate(
+      this.startDate.getDate() + renewalPeriods[this.frequency]
+    );
+
+    if (this.renewalDate <= new Date()) {
+      this.status = "expired";
+    }
+
+    next();
+  }
+});
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+export default Subscription;
